fe9814651610aa70327cfde0566a1ae3a5466985
Peter Levinsky <pele@zealand.dk>
2024-02-06 13:02:03 +0100

    Læsning af konfig fil

---
+++ KonfigIndlæsnin/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+
+Console.WriteLine("Hello, World!");
+
+
+XmlDocument xml = new XmlDocument();
+xml.Load("ConfigFile.xml");
+
+Console.WriteLine("Name");
+XmlNode? nameNode = xml.DocumentElement.SelectSingleNode("Name");
+if (nameNode is not null)
+{
+    string nameStr = nameNode.InnerText.Trim();
+    Console.WriteLine( "Name: " + nameStr );
+}
+
+Console.WriteLine("Number");
+XmlNode? numberNode = xml.DocumentElement.SelectSingleNode("Number");
+if (numberNode is not null)
+{
+    string numberStr = numberNode.InnerText.Trim();
+    int number = int.Parse(numberStr);
+
+    Console.WriteLine("Number: " + number);
+}
